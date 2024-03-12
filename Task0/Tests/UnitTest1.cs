1d4b8bae05ac8fa34c912bed9df8c87d8f170d16
Condotierro <118808096+Condotierro@users.noreply.github.com>
2024-03-12 16:40:40 +0100

    UnitTests, References

--- Task0/Tests/UnitTest1.cs
+++ Task0/Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 namespace Tests;
+using Task0;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 [TestClass]
 public class UnitTest1
@@ -6,5 +8,8 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
+        FactorialCalculator calculator = new FactorialCalculator();
+
+        Assert.AreEqual(24, calculator.Calculate(4));
     }
 }
